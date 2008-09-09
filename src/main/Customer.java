9373c70d0c9f8a8fc1b2fba995b3dc7096ca81c2
unknown <java@.(none)>
2008-09-09 14:20:25 +0200

    costumor hinzugefuegt

---
+++ src/main/Customer.java
@@ -0,0 +1,26 @@
+package main;
+
+import java.util.Date;
+import java.util.List;
+
+ 
+
+class Customer {
+	
+	int cID;
+	String firstname;
+	String lastName;
+	Date birthDate;
+	String street;
+	String houseNr;
+	int zipCode;
+	String City;
+	String identificationNr;
+	String title;
+	List rentList;
+	
+	
+	
+	
+
+}
