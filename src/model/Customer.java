6924512ae858916f37aa925fce05047cf09f25b8
Andie <andhoffm@uos.de>
2008-09-11 16:31:10 +0200

    alle check-Methoden geändert und Konstruktoren angepasst

--- src/model/Customer.java
+++ src/model/Customer.java
@@ -13,58 +13,59 @@ import java.util.Map;
 public class Customer
 {
 
-	private int cID;
+	private final int NotSet = -1;
+	private int cID = NotSet;
 	private String firstName;
 	private String lastName;
 	private Date birthDate;
-	private int age;
+	private int age = NotSet;
 	private String street;
 	private String houseNr;
-	private int zipCode;
+	private int zipCode = NotSet;
 	private String city;
 	private String identificationNr;
 	private String title;
 	private List<InRent> rentList;
-	private final int NotSet = -1;
 
 	private static int mincID;
 	private static Map<Integer, Customer> customerList;
 
+	
 	public Customer(String firstName, String lastName, int yearOfBirth,
-			int monthOfBirth, int dateOfBirth, String street, String houseNr,
+			int monthOfBirth, int dayOfBirth, String street, String houseNr,
 			int zipCode, String city, String identificationNr, String title)
 			throws FalseIDException, EmptyFieldException,
 			FalseBirthDateException, CurrentDateException
 	{
 		this(mincID, firstName, lastName, yearOfBirth, monthOfBirth,
-				dateOfBirth, street, houseNr, zipCode, city, identificationNr,
+				dayOfBirth, street, houseNr, zipCode, city, identificationNr,
 				title);
 		mincID++;
 	}
 
 	private Customer(int cID, String firstName, String lastName,
-			int yearOfBirth, int monthOfBirth, int dateOfBirth, String street,
+			int yearOfBirth, int monthOfBirth, int dayOfBirth, String street,
 			String houseNr, int zipCode, String city, String identificationNr,
 			String title) throws FalseIDException, EmptyFieldException,
 			FalseBirthDateException, CurrentDateException
 	{
-
-		this.zipCode = NotSet;
-		this.cID = cID;
-		this.firstName = firstName;
-		this.lastName = lastName;
-		this.birthDate = new Date(yearOfBirth, monthOfBirth, dateOfBirth);
-		this.street = street;
-		this.houseNr = houseNr;
-		this.zipCode = zipCode;
-		this.city = city;
-		this.identificationNr = identificationNr;
-		this.title = title;
-		this.rentList = new LinkedList<InRent>();
-
-		checkcID();
-		checkEmptyFields();
-		age = checkBirthDate();
+		Date newDate = new Date(yearOfBirth, monthOfBirth, dayOfBirth);
+		
+		if( correctID(cID)	&&	noEmptyFields(firstName, lastName, yearOfBirth, monthOfBirth, dayOfBirth, 
+				street, houseNr, zipCode, city,	identificationNr, title)	&&	correctBirthDate(newDate))
+		{	
+			this.cID = cID;
+			this.firstName = firstName;
+			this.lastName = lastName;
+			this.birthDate = newDate;
+			this.street = street;
+			this.houseNr = houseNr;
+			this.zipCode = zipCode;
+			this.city = city;
+			this.identificationNr = identificationNr;
+			this.title = title;
+			this.rentList = new LinkedList<InRent>();
+		}
 	}
 
 	public static Customer reCreate(int cID, String firstName, String lastName,
@@ -78,31 +79,36 @@ public class Customer
 				identificationNr, title);
 	}
 
-	private void checkcID() throws FalseIDException
+	private boolean correctID(int newcID) throws FalseIDException
 	{
-		if (this.cID < 1)
+		if ( newcID <1 )
 			throw new FalseIDException("cID kleiner 1!");
+		else return true;
 	}
 
-	private void checkEmptyFields() throws EmptyFieldException
+	private boolean noEmptyFields( String firstName, String lastName, int yearOfBirth,
+			int monthOfBirth, int dayOfBirth, String street, String houseNr,
+			int zipCode, String city, String identificationNr, String title ) throws EmptyFieldException
 	{
-		if (this.firstName == null || this.firstName == ""
-				|| this.lastName == null || this.lastName == ""
-				|| this.street == null || this.street == ""
-				|| this.houseNr == null || this.houseNr == ""
-				|| this.city == null || this.city == "" || this.title == null
-				|| this.title == "" || this.identificationNr == null
-				|| this.identificationNr == "" || this.rentList == null
-				|| this.zipCode == NotSet || this.birthDate == null)
+		if ( firstName == null || firstName == ""
+			|| lastName == null || lastName == ""
+			|| yearOfBirth == 0 || monthOfBirth == 0 || dayOfBirth == 0
+			|| street == null || street == ""
+			|| houseNr == null || houseNr == ""
+			|| zipCode == 0
+			|| city == null || city == ""
+			|| identificationNr == null	|| identificationNr == "" 
+			|| title == null|| title == "" 
+			|| rentList == null )
 			throw new EmptyFieldException();
+		else return true;
 	}
 
-	private int checkBirthDate() throws FalseBirthDateException,
-			CurrentDateException
+	private boolean correctBirthDate(Date date) throws FalseBirthDateException,	CurrentDateException
 	{
-		int birthYear = this.birthDate.getYear();
-		int birthMonth = this.birthDate.getMonth();
-		int birthDay = this.birthDate.getDate();
+		int birthYear = date.getYear();
+		int birthMonth = date.getMonth();
+		int birthDay = date.getDate();
 
 		int currentYear = CurrentDate.get().getYear();
 		int currentMonth = CurrentDate.get().getMonth();
@@ -122,8 +128,10 @@ public class Customer
 
 		if (diffYear < 16)
 			throw new FalseBirthDateException("Kunde unter 16");
+		
+		this.age = diffYear;
 
-		return diffYear;
+		return true;
 	}
 
 	public static void setMinID(int newMincID)
@@ -131,9 +139,11 @@ public class Customer
 		mincID = newMincID;
 	}
 
-	public int getAge() throws FalseBirthDateException, CurrentDateException
+	public int getAge() throws FalseBirthDateException, CurrentDateException, EmptyFieldException
 	{
-		return checkBirthDate();
+		if( this.age != NotSet )
+			return this.age;
+		else throw new EmptyFieldException("Kein Alter vorhanden");
 	}
 
 	public int getID()
@@ -180,6 +190,92 @@ public class Customer
 		else
 			throw new EmptyFieldException("Kein Geburtstag vorhanden");
 	}
+	
+	public void setBirthDate(Date newBirthDate) throws EmptyFieldException, FalseBirthDateException, CurrentDateException
+	{
+		if( newBirthDate == null ) throw new EmptyFieldException("Kein Geburtstag eingegeben");
+		
+		else
+		{
+			if( correctBirthDate(newBirthDate) )
+			{
+				this.birthDate.setYear(newBirthDate.getYear());
+				this.birthDate.setMonth(newBirthDate.getMonth());
+				this.birthDate.setDate(newBirthDate.getDate());
+			}
+		}
+	}
+	
+	public String getStreet() throws EmptyFieldException
+	{
+		if( this.street != null && this.street != "")
+			return street;
+		else throw new EmptyFieldException("Keine Straße vorhanden");
+	}
+	
+	public void setStreet(String newStreet) throws EmptyFieldException
+	{
+		if( newStreet != null && newStreet != "")
+			this.street = newStreet;
+		else throw new EmptyFieldException("Keine Straße eingegeben");
+	}
+
+	public String getHouseNr() throws EmptyFieldException
+	{
+		if( this.houseNr != null && this.houseNr != "")
+			return this.houseNr;
+		else throw new EmptyFieldException("Keine Hausnummer vorhanden");
+	}	
+	
+	public void setHouseNr(String newHouseNr) throws EmptyFieldException
+	{
+		if( newHouseNr != null && newHouseNr != "")
+			this.houseNr = newHouseNr;
+		else throw new EmptyFieldException("Keine Hausnummer eingegeben");
+	}	
+
+	public int getZipCode() //throws FalseFieldException
+	{
+		//if( this.zipCode > 1 )
+			return this.zipCode;
+		//else throw new FalseFieldException("Keine PLZ vorhanden");
+	}
+	
+	public void setZipCode(int newZipCode) throws FalseFieldException
+	{
+		if( newZipCode < 1 )
+			this.zipCode = newZipCode;
+		else throw new FalseFieldException("Keine/falsche PLZ eingegeben");
+	}
+	
+	public String getCity() throws EmptyFieldException
+	{
+		if( this.city != null && this.city != "")
+			return city;
+		else throw new EmptyFieldException("Keine Stadt vorhanden");
+	}
+	
+	public void setCity(String newCity) throws EmptyFieldException
+	{
+		if( newCity != null && newCity != "")
+			this.city = newCity;
+		else throw new EmptyFieldException("Keine Stadt eingegeben");
+	}
+	
+	public String getIdentificationNr() throws EmptyFieldException
+	{
+		if( this.identificationNr != null && this.identificationNr != "")
+			return this.identificationNr;
+		else throw new EmptyFieldException("Keine Personalausweisnummer vorhanden");
+	}
+	
+	public void setIdentificationNr(String newIdentificationNr) throws EmptyFieldException
+	{
+		if( newIdentificationNr != null && newIdentificationNr != "")
+			this.identificationNr = newIdentificationNr;
+		else throw new EmptyFieldException("Keine Personalausweisenummer eingegeben");
+	}
+	
 
 	public static Customer findByID(int customerID)
 			throws RecordNotFoundException
@@ -222,12 +318,6 @@ public class Customer
 		throw new RecordNotFoundException("Kunde", "Geburtsdatum", DateFormat
 				.getInstance().format(birthDate));
 	}
-
-	/**
-	 * public void setBirthDate(Date newBirthDate){ if( newBirthDate != null ){
-	 * this.birthDate } }
-	 * 
-	 */
 
 	public static void setCustomerList(Map<Integer, Customer> newCustomerList)
 	{
