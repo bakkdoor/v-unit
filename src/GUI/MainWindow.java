85ad97dc77bb8ac79c56bb216e714a706748f096
unknown <java@.(none)>
2008-09-11 12:02:48 +0200

    in Methoden Ausgelagert

--- src/GUI/MainWindow.java
+++ src/GUI/MainWindow.java
@@ -3,6 +3,7 @@ package GUI;
 import java.awt.BorderLayout;
 import java.awt.GridBagConstraints;
 import java.awt.GridBagLayout;
+import java.awt.Insets;
 import java.awt.event.WindowAdapter;
 import java.awt.event.WindowEvent;
 
@@ -17,6 +18,7 @@ import javax.swing.JMenu;
 import javax.swing.JMenuBar;
 import javax.swing.JMenuItem;
 import javax.swing.JPanel;
+import javax.swing.JScrollPane;
 import javax.swing.JSeparator;
 import javax.swing.JTable;
 import javax.swing.JTextField;
@@ -55,7 +57,43 @@ public class MainWindow {
 		
 		// **************************************************************
 		// MenuBar
+		gridBagConstCentral.gridx = 0;
+		gridBagConstCentral.gridy = 0;
+//		gridBagConstCentral.gridwidth = 2;
+		gridBagConstCentral.fill = GridBagConstraints.HORIZONTAL;
+		gridBagConstCentral.anchor = GridBagConstraints.FIRST_LINE_START;
+		panelCentral.add(this.createMenuBar(), gridBagConstCentral);
+		
+		// **************************************************************
+		// Toolbar mit Buttons
+		gridBagConstCentral.gridx = 0;
+		gridBagConstCentral.gridy = 1;
+		gridBagConstCentral.fill = GridBagConstraints.HORIZONTAL;
+		panelCentral.add(this.createToolBar(), gridBagConstCentral);
+		
+		// **************************************************************
+		// Ausleihe
+		gridBagConstCentral.gridx = 0;
+		gridBagConstCentral.gridy = 2;
+		gridBagConstCentral.gridwidth = 1;
+		gridBagConstCentral.fill = GridBagConstraints.HORIZONTAL;
+		panelCentral.add(this.createRentPanel(), gridBagConstCentral);
+		
+		// ***************************************************************
+		
+		mainFrame.add(this.createRentPanel());
+//		mainFrame.add(panelCentral);
+//		mainFrame.setSize(800,600);
+		mainFrame.pack();
+		mainFrame.setVisible(true);
+	}
+	
+	private JMenuBar createMenuBar() {
+		
+		// MenuBar
 		JMenuBar menuBarMain = new JMenuBar();
+		
+		
 		// Menu Kunde
 		JMenu menuCustomer = new JMenu("Kunde");
 		JMenuItem menuItemCustomerNew = new JMenuItem("aufnehmen");
@@ -65,6 +103,7 @@ public class MainWindow {
 		menuCustomer.add(menuItemCustomerNew);
 		menuCustomer.add(menuItemCustomerEdit);
 		menuCustomer.add(menuItemCustomerDelete);
+		
 		
 		// Menu Video
 		JMenu menuVideo = new JMenu("Video");
@@ -76,13 +115,15 @@ public class MainWindow {
 		menuVideo.add(menuItemVideoEdit);
 		menuVideo.add(menuItemVideoDelete);
 		
+		
 		// Menu Suchen
-		JMenu menuSearch = new JMenu("Suche ???");
+		JMenu menuSearch = new JMenu("Suche");
 		JMenuItem menuItemSearchCustomer = new JMenuItem("nach Kunde");
 		JMenuItem menuItemSearchVideo = new JMenuItem("nach Video");
 		
 		menuSearch.add(menuItemSearchCustomer);
 		menuSearch.add(menuItemSearchVideo);
+		
 		
 		// Menu Extras
 		JMenu menuTools = new JMenu("Extras");
@@ -95,6 +136,7 @@ public class MainWindow {
 		menuTools.add(separatorToolsSeparator);
 		menuTools.add(menuItemToolsOptions);
 		
+		
 		// Menu Hilfe
 		JMenu menuHelp = new JMenu("Hilfe");
 		JMenuItem menuItemHelpGetHelp = new JMenuItem("Hilfetext");
@@ -104,26 +146,24 @@ public class MainWindow {
 		menuHelp.add(menuItemHelpAbout);
 		
 		
+		// Menus in dei MenuBar hinzufügen
 		menuBarMain.add(menuCustomer);
 		menuBarMain.add(menuVideo);
 		menuBarMain.add(menuSearch);
 		menuBarMain.add(menuTools);
 		menuBarMain.add(menuHelp);
 		
-		gridBagConstCentral.gridx = 0;
-		gridBagConstCentral.gridy = 0;
-//		gridBagConstCentral.gridwidth = 2;
-		gridBagConstCentral.fill = GridBagConstraints.HORIZONTAL;
-		gridBagConstCentral.anchor = GridBagConstraints.FIRST_LINE_START;
-		panelCentral.add(menuBarMain, gridBagConstCentral);
+		return menuBarMain;
+	}
+	
+	private JToolBar createToolBar() {
 		
-		// **************************************************************
-		
-		// Toolbar mit Buttons
+		// ToolBar erzeugen
 		JToolBar toolBarButtons = new JToolBar();
 		toolBarButtons.setRollover(true);	// Aussehehen nicht wie ein Button
 		toolBarButtons.setFloatable(false);	// Toolbar nicht verschiebbar
 		toolBarButtons.setBorderPainted(true);
+		
 		
 		// Button Suche
 		JButton toolBarButtonSearch = new JButton("Suchen");
@@ -171,82 +211,166 @@ public class MainWindow {
 		JSeparator separatorToolBar2 = new JSeparator(JSeparator.VERTICAL);
 		
 		
+		// Buttons der ToolBar hinzufügen
 		toolBarButtons.add(toolBarButtonSearch);
 //		toolBarButtons.add(separatorToolBar1);
 		toolBarButtons.add(toolBarButtonNew);
 		toolBarButtons.add(toolBarButtonEdit);
 		toolBarButtons.add(toolBarButtonDelete);
 //		toolBarButtons.add(separatorToolBar2);
-		toolBarButtons.setVisible(true);
 		
-		gridBagConstCentral.gridx = 0;
-		gridBagConstCentral.gridy = 1;
-		gridBagConstCentral.fill = GridBagConstraints.HORIZONTAL;
-		panelCentral.add(toolBarButtons, gridBagConstCentral);
+		return toolBarButtons;
+	}
+	
+	private JPanel createRentPanel() {
 		
-		// **************************************************************
-		// Ausleihe 
-		GridBagLayout layoutRent = new GridBagLayout();
-        GridBagConstraints layoutRentConst = new GridBagConstraints();
-		JPanel panelRent = new JPanel(layoutRent);
+		// Panel erstellen
+		JPanel panelRent = new JPanel(new GridBagLayout());
+		GridBagConstraints gridBagConstRent = new GridBagConstraints();
+		
 
-		
+		// KundenNr - Label/TextField erstellen
 		JLabel labelRentCustomer = new JLabel("KundenNr.:");
 		JTextField textFieldRentCustomer = new JTextField();
 		
+		// VideoNr - Label/TextField erstellen
 		JLabel labelRentVideo = new JLabel("FilmNr.:");
 		JTextField textFieldRentVideo = new JTextField();
 		
+		// Ausleihdauer - Label erstellen
 		JLabel labelRentDuration = new JLabel("Ausleihdauer:");
 		comboBoxRentDuration = new JComboBox(comboBoxRentDurationContent);
 		
+		// Hinzufügen Button erstellen
 		JButton buttonRentAdd = new JButton("Hinzufügen");
 		
+		// LeihVideo Tabelle erstellen
 		tableModelRentVideo = 
 			new DefaultTableModel(stringArrTableModelRentVidCont, stringArrTableModelRentVidCollNames);
 		tableRentVideo = new JTable(tableModelRentVideo);
 		
+		// Abbrechen/Akzeptieren Button erstellen
 		JButton buttonRentCancel = new JButton("Abbrechen");
 		JButton buttonRentAccept = new JButton("Abschließen");
 		
+		
 		// Datenfelder in panelRent einfügen
-		layoutRentConst.weightx = 1.0;
-		layoutRent.setConstraints(labelRentCustomer,layoutRentConst);
-		panelRent.add(labelRentCustomer);
+		// Label KundenNr
+		gridBagConstRent.gridx = 0;
+		gridBagConstRent.gridy = 0;
+		gridBagConstRent.weightx = 0.3;
+		gridBagConstRent.weighty = 0.0;
+		gridBagConstRent.fill = GridBagConstraints.HORIZONTAL;
+		gridBagConstRent.anchor = GridBagConstraints.BELOW_BASELINE;
+		gridBagConstRent.insets = new Insets(3,3,0,0);
+		panelRent.add(labelRentCustomer, gridBagConstRent);
 		
-		layoutRentConst.gridwidth = GridBagConstraints.REMAINDER;
-		layoutRentConst.ipadx = 200;
-		layoutRent.setConstraints(textFieldRentCustomer,layoutRentConst);
-		panelRent.add(textFieldRentCustomer);
+		// TextField KundenNr
+		gridBagConstRent.gridx = 2;
+		gridBagConstRent.gridy = 0;
+		gridBagConstRent.weightx = 0.3;
+		gridBagConstRent.weighty = 0.0;
+		gridBagConstRent.fill = GridBagConstraints.HORIZONTAL;
+		gridBagConstRent.anchor = GridBagConstraints.BELOW_BASELINE;
+		gridBagConstRent.insets = new Insets(3,0,0,3);
+		panelRent.add(textFieldRentCustomer, gridBagConstRent);
 		
-		layoutRentConst.weightx = 1.0;
-		layoutRent.setConstraints(labelRentVideo,layoutRentConst);
-		panelRent.add(labelRentVideo);
+		// Label VideoNr
+		gridBagConstRent.gridx = 0;
+		gridBagConstRent.gridy = 1;
+		gridBagConstRent.weightx = 0.3;
+		gridBagConstRent.weighty = 0.0;
+		gridBagConstRent.fill = GridBagConstraints.HORIZONTAL;
+		gridBagConstRent.anchor = GridBagConstraints.BELOW_BASELINE;
+		gridBagConstRent.insets = new Insets(0,3,0,0);
+		panelRent.add(labelRentVideo, gridBagConstRent);
 		
-		layoutRentConst.gridwidth = GridBagConstraints.REMAINDER;
-		layoutRent.setConstraints(textFieldRentVideo,layoutRentConst);
-		panelRent.add(textFieldRentVideo);
+		// TextField VideoNr
+		gridBagConstRent.gridx = 2;
+		gridBagConstRent.gridy = 1;
+		gridBagConstRent.weightx = 0.3;
+		gridBagConstRent.weighty = 0.0;
+		gridBagConstRent.fill = GridBagConstraints.HORIZONTAL;
+		gridBagConstRent.anchor = GridBagConstraints.BELOW_BASELINE;
+		gridBagConstRent.insets = new Insets(0,0,0,3);
+		panelRent.add(textFieldRentVideo, gridBagConstRent);
 		
-		layoutRentConst.weightx = 1.0;
-		layoutRent.setConstraints(labelRentDuration,layoutRentConst);
-		panelRent.add(labelRentDuration);
+		// Label Dauer
+		gridBagConstRent.gridx = 0;
+		gridBagConstRent.gridy = 2;
+		gridBagConstRent.weightx = 0.3;
+		gridBagConstRent.weighty = 0.0;
+		gridBagConstRent.fill = GridBagConstraints.HORIZONTAL;
+		gridBagConstRent.anchor = GridBagConstraints.BELOW_BASELINE;
+		gridBagConstRent.insets = new Insets(0,3,0,0);
+		panelRent.add(labelRentDuration, gridBagConstRent);
 		
-		layoutRentConst.gridwidth = GridBagConstraints.REMAINDER;
-		layoutRent.setConstraints(comboBoxRentDuration,layoutRentConst);
-		panelRent.add(comboBoxRentDuration);
+		// ComboBox Dauer
+		gridBagConstRent.gridx = 2;
+		gridBagConstRent.gridy = 2;
+		gridBagConstRent.weightx = 0.3;
+		gridBagConstRent.weighty = 0.0;
+		gridBagConstRent.fill = GridBagConstraints.HORIZONTAL;
+		gridBagConstRent.anchor = GridBagConstraints.BELOW_BASELINE;
+		gridBagConstRent.insets = new Insets(0,0,0,3);
+		panelRent.add(comboBoxRentDuration, gridBagConstRent);
 		
-		// panelRent in panelCentral
-		gridBagConstCentral.gridx = 0;
-		gridBagConstCentral.gridy = 2;
-		gridBagConstCentral.gridwidth = 1;
-		gridBagConstCentral.fill = GridBagConstraints.HORIZONTAL;
-		panelCentral.add(panelRent, gridBagConstCentral);
-		// ***************************************************************
+		// Button Hinzufügen
+		gridBagConstRent.gridx = 1;
+		gridBagConstRent.gridy = 3;
+		gridBagConstRent.gridwidth = 2;
+		gridBagConstRent.weightx = 0.3;
+		gridBagConstRent.weighty = 0.0;
+		gridBagConstRent.fill = GridBagConstraints.HORIZONTAL;
+		gridBagConstRent.anchor = GridBagConstraints.BELOW_BASELINE;
+		gridBagConstRent.insets = new Insets(3,3,3,3);
+		panelRent.add(buttonRentAdd, gridBagConstRent);
 		
-		mainFrame.add(panelCentral);
+		// Tabelle mit Ausleihfilmen
+		gridBagConstRent.gridx = 0;
+		gridBagConstRent.gridy = 4;
+		gridBagConstRent.ipady = 60;
+		gridBagConstRent.gridwidth = 3;
+		gridBagConstRent.gridheight = 6;
+		gridBagConstRent.weightx = 0.3;
+		gridBagConstRent.weighty = 1.0;
+		gridBagConstRent.fill = GridBagConstraints.BOTH;
+		gridBagConstRent.anchor = GridBagConstraints.BELOW_BASELINE;
+		gridBagConstRent.insets = new Insets(3,3,3,3);
+		panelRent.add(tableRentVideo, gridBagConstRent);
 		
-		mainFrame.setSize(800,600);
-		mainFrame.setVisible(true);
+		// Button Abbrechen
+		gridBagConstRent.gridx = 1;
+		gridBagConstRent.gridy = 10;
+		gridBagConstRent.ipady = 0;
+		gridBagConstRent.gridwidth = 1;
+		gridBagConstRent.gridheight = 1;
+		gridBagConstRent.weightx = 0.3;
+		gridBagConstRent.weighty = 0.0;
+		gridBagConstRent.fill = GridBagConstraints.HORIZONTAL;
+		gridBagConstRent.anchor = GridBagConstraints.BELOW_BASELINE;
+		gridBagConstRent.insets = new Insets(3,0,0,3);
+		panelRent.add(buttonRentCancel, gridBagConstRent);
+		
+		// Button Akzeptieren
+		gridBagConstRent.gridx = 2;
+		gridBagConstRent.gridy = 10;
+		gridBagConstRent.weightx = 0.3;
+		gridBagConstRent.weighty = 0.0;
+		gridBagConstRent.fill = GridBagConstraints.HORIZONTAL;
+		gridBagConstRent.anchor = GridBagConstraints.BELOW_BASELINE;
+		gridBagConstRent.insets = new Insets(3,3,0,3);
+		panelRent.add(buttonRentAccept, gridBagConstRent);
+		
+		return panelRent;
 	}
-
+	
+	private JPanel createDetailPanel() {
+		return null;
+	}
+	
+	private JPanel createTablePanel() {
+		return null;
+	}
+	
 }
