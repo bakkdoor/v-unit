6f473f239cf8c00f97f3cc773933ef880a2043f8
Andie <andhoffm@uos.de>
2008-09-11 12:34:34 +0200

    nur CurrentDateException erweitert

--- src/model/exceptions/CurrentDateException.java
+++ src/model/exceptions/CurrentDateException.java
@@ -3,9 +3,16 @@ package model.exceptions;
 import main.error.VideothekException;
 
 public class CurrentDateException extends VideothekException {
-	
+
 	public CurrentDateException(String message){
 		super(message);
 	}
+	
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = -2498429645495210193L;
+
+
 
 }
