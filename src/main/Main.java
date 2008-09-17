18b07bc98678341c329a9d14609ec8caeafce83b
Christopher Bertels <chbertel@uos.de>
2008-09-17 15:37:33 +0200

    main & programm klassen ueberarbeitet

--- src/main/Main.java
+++ src/main/Main.java
@@ -23,32 +23,6 @@ import model.exceptions.EmptyFieldException;
  */
 public class Main{
 	public static void main(String[] args) throws InterruptedException, EmptyFieldException, RecordNotFoundException{
-		Logger.get().write("Programm gestartet!");
-		
-		Logger.get().write("CurrentDate wird gesetzt");
-		
-		try
-		{
-			CurrentDate.set(new Date());
-		}
-		catch (CurrentDateException e2)
-		{
-			Logger.get().write("Fehler beim Setzen von CurrentDate!");
-		}
-
-		Logger.get().write("XML-Daten werden geladen.");
-		
-		try
-		{
-			DataBase.loadData();
-			Logger.get().write("Daten erfolgreich geladen.");
-		}
-		catch (DataException e1)
-		{
-			Logger.get().write("Es gab einen Fehler beim Laden!");
-		}
-
-		Programm.start(new MainWindow());
-		
+		Programm.start();
 	}
 }
--- src/main/Programm.java
+++ src/main/Programm.java
@@ -6,27 +6,57 @@ package main;
 import java.io.IOException;
 
 import logging.Logger;
+import model.CurrentDate;
+import model.Date;
+import model.data.DataBase;
+import model.data.exceptions.DataException;
+import model.exceptions.CurrentDateException;
 import GUI.MainWindow;
 
 /**
  * @author java
- *
+ * 
  */
-public class Programm {
+public class Programm
+{
 
 	private static MainWindow mainWindow;
-	
-	public static void start(MainWindow window){
-		mainWindow = window;
-		window.getMainFrame().setVisible(true);
+
+	public static void start()
+	{
+		Logger.get().write("Programm gestartet!");
+		Logger.get().write("CurrentDate wird gesetzt");
+		try
+		{
+			CurrentDate.set(new Date());
+		}
+		catch (CurrentDateException e2)
+		{
+			Logger.get().write("Fehler beim Setzen von CurrentDate!");
+		}
+
+		Logger.get().write("XML-Daten werden geladen.");
+
+		try
+		{
+			DataBase.loadData();
+			Logger.get().write("Daten erfolgreich geladen.");
+		}
+		catch (DataException e1)
+		{
+			Logger.get().write("Es gab einen Fehler beim Laden!");
+		}
+
+		mainWindow = new MainWindow();
+		mainWindow.getMainFrame().setVisible(true);
 	}
-	
+
 	public static void shutdown()
 	{
 		mainWindow.getMainFrame().dispose();
-		
+
 		Logger.get().write("Programm wird beendet!");
-		
+
 		try
 		{
 			Logger.get().close();
@@ -35,7 +65,7 @@ public class Programm {
 		{
 			e.printStackTrace();
 		}
-		
+
 		// TODO: abspeichern von daten, andere sachen aufräumen etc.
 	}
 }
