cfd39f42ad93e2443dfa1d25761bf9d2f2bf7c7d
Christopher Bertels <chbertel@uos.de>
2008-09-22 18:21:46 +0200

    SetCurrentDate-Dialog hinzugefuegt. Erscheint am Anfang, sodass man das aktuelle tagesdatum einstellen kann

---
+++ src/model/events/SetCurrentDateEvent.java
@@ -0,0 +1,12 @@
+package model.events;
+
+/**
+ * SetCurrentDateEvent.java
+ * @author Christopher Bertels (chbertel@uos.de)
+ * 22.09.2008
+ * 
+ * Wird gefeuert, wenn der OK-Button im SetCurrentDate-Dialog geklickt wurde.
+ */
+public class SetCurrentDateEvent extends VideothekEvent
+{
+}
