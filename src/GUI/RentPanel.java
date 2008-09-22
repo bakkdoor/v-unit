e6185a05a23d4ff5b9c168fd31603a4aa5dffa3d
Christopher Bertels <chbertel@uos.de>
2008-09-22 16:36:40 +0200

    border-title sind jetzt groesser zur besseren uebersicht

--- src/GUI/RentPanel.java
+++ src/GUI/RentPanel.java
@@ -3,6 +3,7 @@ package GUI;
 import java.awt.CardLayout;
 import java.awt.Component;
 import java.awt.Container;
+import java.awt.Font;
 import java.awt.GridBagConstraints;
 import java.awt.GridBagLayout;
 import java.awt.Insets;
@@ -18,6 +19,7 @@ import javax.swing.JPanel;
 import javax.swing.JScrollPane;
 import javax.swing.JTable;
 import javax.swing.JTextField;
+import javax.swing.border.TitledBorder;
 import javax.swing.table.DefaultTableModel;
 import javax.swing.table.TableModel;
 import javax.swing.table.TableRowSorter;
@@ -65,7 +67,9 @@ public class RentPanel {
 		JPanel panelRent = new JPanel(new GridBagLayout());
 		GridBagConstraints gridBagConstRent = new GridBagConstraints();
 
-		panelRent.setBorder(BorderFactory.createTitledBorder("Ausleihe"));
+		TitledBorder border = BorderFactory.createTitledBorder("Ausleihe");
+		border.setTitleFont(new Font("Arial", Font.BOLD, 14));
+		panelRent.setBorder(border);
 
 		// KundenNr - Label/TextField erstellen
 		JLabel labelRentCustomer = new JLabel("KundenNr.:");
@@ -215,7 +219,11 @@ public class RentPanel {
 
 	private Container returnVideoPanel() {
 		JPanel returnVideoPanel = new JPanel();
-		returnVideoPanel.setBorder(BorderFactory.createTitledBorder("Rückgabe"));
+		
+		TitledBorder border = BorderFactory.createTitledBorder("Rückgabe");
+		border.setTitleFont(new Font("Arial", Font.BOLD, 14));
+		returnVideoPanel.setBorder(border);
+		
 		returnVideoPanel.setLayout(new GridBagLayout());
 		
 		JLabel labelReturnVideo = new JLabel("ExemplarNr.:");
