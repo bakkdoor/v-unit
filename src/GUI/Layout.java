35641308ea89559f06bd6b67b4ea9be5515be660
Waldemar Smirnow <wmsirnow@uos.de>
2008-09-25 17:15:09 +0200

    Layout dokumentiert

--- src/GUI/Layout.java
+++ src/GUI/Layout.java
@@ -6,19 +6,30 @@ import java.awt.GridBagConstraints;
 import java.awt.Insets;
 
 public class Layout {
-	
-	// Ausrichtungswerte
-	private final int  HORIZONTAL = GridBagConstraints.HORIZONTAL;
-	private final int  VERTICAL = GridBagConstraints.VERTICAL;
-	private final int  BOTH = GridBagConstraints.BOTH;
-	private final int  BELOW_BASELINE = GridBagConstraints.BELOW_BASELINE;
-	private final int  NORTHWEST = GridBagConstraints.NORTHWEST;
 
+	/**
+	 * Fügt die übergebene Komponennte in den übergebenen Kontainer an der Pos. x,y ein
+	 * @param container zu füllender Kontainer
+	 * @param component die einzusetzende Komponennte
+	 * @param x Pos x
+	 * @param y Posy
+	 */
 	public static void addComponent(Container container, Component component, 	
 			int x, int y) {
 		Layout.addComponent(container, component, x, y, 1, 1, 0.0, 0.0);	
 	}
 	
+	/**
+	 * Fügt die übergebene Komponennte in den übergebenen Kontainer an der Pos. x,y  und weiteren übergebenen Werten ein
+	 * @param container zu füllender Kontainer
+	 * @param component die einzusetzende Komponennte
+	 * @param x Pos x
+	 * @param y Pos y
+	 * @param gridwidth einzunehmende Zeilen#
+	 * @param gridheight einzunehmende Spalten#
+	 * @param widthx analog zu GridBagConstraints widthx
+	 * @param widthy analog zu GridGagConstraints widthy
+	 */
 	public static void addComponent(Container container, Component component, 	
 									int x, int y, 
 									int gridwidth, int gridheight,
@@ -26,7 +37,22 @@ public class Layout {
 	
 		addComponent(container, component, x, y, gridwidth, gridheight, widthx, widthy, 0, 0, GridBagConstraints.HORIZONTAL, GridBagConstraints.BELOW_BASELINE, new Insets(3, 3, 3, 3));
 	}
-	
+	/**
+	 * Fügt die übergebene Komponennte in den übergebenen Kontainer an der Pos. x,y  und weiteren übergebenen Werten ein
+	 * @param container zu füllender Kontainer
+	 * @param component die einzusetzende Komponennte
+	 * @param x Pos x
+	 * @param y Pos y
+	 * @param gridwidth einzunehmende Zeilen#
+	 * @param gridheight einzunehmende Spalten#
+	 * @param widthx analog zu GridBagConstraints widthx
+	 * @param widthy analog zu GridBagConstraints widthy
+	 * @param ipadx analog zu GridBagConstraints ipadx
+	 * @param ipady analog zu GridBagConstraints ipady
+	 * @param fill analog zu GridBagConstraints fill
+	 * @param anchor analog zu GridBagConstraints anchor
+	 * @param insets Insets Objekt
+	 */
 	public static void addComponent(Container container, Component component,
 									int x, int y, 
 									int gridwidth, int gridheight, 
